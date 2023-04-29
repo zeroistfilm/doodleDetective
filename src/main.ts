f3a804835bd4a8c2f8da8022433728999f253cf9
김영동 <zeroistfilm@naver.com>
2023-04-30 00:31:28 +0900

    cors 추가

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,20 @@ async function bootstrap() {
 
   setupSwagger(app);
   app.use(json({ limit: '50mb' }));
+  app.enableCors({
+    origin: [
+      'http://127.0.0.1:3000',
+      'http://localhost:3000',
+      'http://localhost:3001',
+      'https://localhost:3000',
+      'https://localhost:3001',
+      'https://localhost:80',
+    ], // 접근 권한을 부여하는 도메인
+    credentials: true, // 응답 헤더에 Access-Control-Allow-Credentials 추가
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS', // 응답 헤더에 Access-Control-Allow-Methods 추가
+    //optionsSuccessStatus: 200, // 응답 상태 200으로 설정
+  });
+
   await app.listen(3000);
 }
 bootstrap();
