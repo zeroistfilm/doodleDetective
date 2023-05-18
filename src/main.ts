a7715593995edae13430fa54842489f1adebff81
youngdongkim <47134564+zeroistfilm@users.noreply.github.com>
2023-05-19 08:11:35 +0900

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,7 @@ async function bootstrap() {
   app.enableCors({
     origin: [
       'http://127.0.0.1:3000',
+      'http://127.0.0.1:5500',
       'http://localhost:3000',
       'http://localhost:3001',
       'https://localhost:3000',
